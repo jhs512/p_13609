a497f48592ef6553ebff3ce0b1e5f75481dad442
jhs512 <jangka512@gmail.com>
2024-07-04 02:53:54 +0900

    work

--- src/main/java/com/spring/app/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/spring/app/domain/post/post/controller/ApiV1PostController.java
@@ -5,13 +5,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/v1/posts")
 public class ApiV1PostController {
     @GetMapping("/{id}")
-    public String getItem(
+    public Map<String, Object> getItem(
             @PathVariable long id
     ) {
-        return "%d번 글 조회".formatted(id);
+        LocalDateTime createDate = LocalDateTime.now();
+        LocalDateTime modifyDate = LocalDateTime.now();
+        String title = "제목 " + id;
+        String body = "내용 " + id;
+
+        return Map.of(
+                "id", id,
+                "createDate", createDate,
+                "modifyDate", modifyDate,
+                "title", title,
+                "body", body
+        );
     }
 }
