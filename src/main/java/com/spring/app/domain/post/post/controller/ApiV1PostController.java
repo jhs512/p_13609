a27a1ad0ccf3f4630b68a0375c01684f49ee12b6
jhs512 <jangka512@gmail.com>
2024-07-04 03:57:24 +0900

    work

--- src/main/java/com/spring/app/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/spring/app/domain/post/post/controller/ApiV1PostController.java
@@ -1,5 +1,6 @@
 package com.spring.app.domain.post.post.controller;
 
+import com.spring.app.global.rsData.RsData;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,7 @@ import java.util.Map;
 @RequestMapping("/api/v1/posts")
 public class ApiV1PostController {
     @GetMapping("/{id}")
-    public Map<String, Object> getItem(
+    public RsData getItem(
             @PathVariable long id
     ) {
         LocalDateTime createDate = LocalDateTime.now();
@@ -20,11 +21,10 @@ public class ApiV1PostController {
         String title = "제목 " + id;
         String body = "내용 " + id;
 
-        return Map.of(
-                "resultCode", "S-200",
-                "statusCode", 200,
-                "msg", "성공",
-                "body", Map.of(
+        return new RsData(
+                "S-200",
+                "성공",
+                Map.of(
                         "id", id,
                         "createDate", createDate,
                         "modifyDate", modifyDate,
---
+++ src/main/java/com/spring/app/global/rsData/RsData.java
@@ -0,0 +1,20 @@
+package com.spring.app.global.rsData;
+
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+public class RsData {
+    private final String resultCode;
+    private final int statusCode;
+    private final String msg;
+    private final Map<String, Object> body;
+
+    public RsData(String resultCode, String msg, Map<String, Object> body) {
+        this.resultCode = resultCode;
+        this.statusCode = Integer.parseInt(resultCode.split("-", 2)[1]);
+        this.msg = msg;
+        this.body = body;
+    }
+}
