1ec3821a32f7a4428be809bfd286e4eb1c7a6ab4
jhs512 <jangka512@gmail.com>
2024-07-04 14:02:17 +0900

    work

--- src/main/java/com/spring/app/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/spring/app/domain/post/post/controller/ApiV1PostController.java
@@ -1,34 +1,25 @@
 package com.spring.app.domain.post.post.controller;
 
+import com.spring.app.domain.post.post.entity.Post;
 import com.spring.app.global.rsData.RsData;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
-import java.util.Map;
-
 @RestController
 @RequestMapping("/api/v1/posts")
 public class ApiV1PostController {
     @GetMapping("/{id}")
-    public RsData<Map<String, Object>> getItem(
+    public RsData<Post> getItem(
             @PathVariable long id
     ) {
-        LocalDateTime createDate = LocalDateTime.now();
-        LocalDateTime modifyDate = LocalDateTime.now();
-        String title = "제목 " + id;
-        String body = "내용 " + id;
+        Post post = Post.builder()
+                .id(id)
+                .title("제목 " + id)
+                .body("내용 " + id)
+                .build();
 
-        return RsData.of(
-                Map.of(
-                        "id", id,
-                        "createDate", createDate,
-                        "modifyDate", modifyDate,
-                        "title", title,
-                        "body", body
-                )
-        );
+        return RsData.of(post);
     }
 }
---
+++ src/main/java/com/spring/app/domain/post/post/entity/Post.java
@@ -0,0 +1,19 @@
+package com.spring.app.domain.post.post.entity;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+public class Post {
+    private Long id;
+    @Builder.Default
+    private LocalDateTime createDate = LocalDateTime.now();
+    @Builder.Default
+    private LocalDateTime modifyDate = LocalDateTime.now();
+    private String title;
+    private String body;
+}
