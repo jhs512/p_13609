b7c8a2d9b6c83342725a8b53c09230464902a832
jhs512 <jangka512@gmail.com>
2024-07-05 11:46:41 +0900

    work

--- src/main/java/com/spring/app/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/spring/app/domain/post/post/controller/ApiV1PostController.java
@@ -2,8 +2,6 @@ package com.spring.app.domain.post.post.controller;
 
 import com.spring.app.domain.post.post.entity.Post;
 import com.spring.app.global.rsData.RsData;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/posts")
 public class ApiV1PostController {
-    @AllArgsConstructor
-    @Getter
-    public static class PostGetItemResBody {
-        private Post item;
+    public record PostGetItemResBody(Post item) {
     }
 
     @GetMapping("/{id}")
